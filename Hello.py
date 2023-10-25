eca4e61c9d404bffffe9150d7cb11716809bb959
SouLKP <142486241+SouLKP@users.noreply.github.com>
2023-10-25 13:58:51 +0530

    Update Hello.py

--- Hello.py
+++ Hello.py
@@ -1,14 +1,100 @@
+import streamlit as st  
+import tempfile
+import pickle
+from dotenv import load_dotenv
+import os
+import openai
+import pickle
+from langchain.text_splitter import CharacterTextSplitter
+from langchain.llms import OpenAI
+from langchain.chains import VectorDBQA
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.document_loaders import TextLoader
+from langchain.vectorstores import FAISS
+from langchain.document_loaders import PyPDFLoader
+from langchain.chains import RetrievalQA
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
 import streamlit as st
-from jemo import *  
+load_dotenv()
+
+MODEL="gpt-3.5-turbo"
+OPENAI_API_KEY = st.secrets["OPENAI_API_KEY"]
+# OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+llm = OpenAI(temperature=0)
+memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+
+    
+
+
+def generate_questions(input_dict):
+    print(input_dict,'\n **********************') 
+    file_path = 'pkl/deepbook.pkl'
+    with open(file_path, 'rb') as file:
+        book_content_vectorstore = pickle.load(file)
+
+    prompt_template = "Generate questions from the book content with the following section-wise instructions:"
+    output_template = ""
+
+    # Iterate through the dynamic dictionary
+    for section, values in input_dict.items():
+        section_name, question_type, difficulty, num_questions,topic  = values
+        prompt_template += f"\n\nSection {section_name}:"
+        prompt_template += f"\nQuestion type: {question_type}"
+        prompt_template += f"\nDifficulty level: {difficulty}"
+        prompt_template += f"\nTotal number of Question : {num_questions}"
+        prompt_template += f"\n Topic : {topic}"
+
+        output_template += f"\n\nSection {section_name}: instruction : {question_type}"
+        output_template += f"\nQuestion: [Generated {question_type} question]"
+        output_template += f"\nAnswer: [Answer option letter]"
+
+    last_message = "\n The questions should demonstrate understanding of key concepts from the book content. Format the full question paper clearly labeling the sections and numbering the questions sequentially."    
+    
+    prompt_template = prompt_template + output_template + last_message
+    llm = ChatOpenAI(model_name=MODEL, temperature=0.4, openai_api_key='')
+    chain = RetrievalQA.from_llm(
+            llm=llm,
+            retriever=book_content_vectorstore.as_retriever(),
+            memory=memory, 
+        )
+
+    question_format_instructions = chain(prompt_template)
+    # print("instruction :  ",question_format_instructions['result'])
+    quepep = question_format_instructions['result']
+    return quepep
 
 if 'my_dict' not in st.session_state:
     st.session_state.my_dict = {}
 
+
 with st.sidebar:
     l1 = []
     st.title("Sectional Details")
-    with st.form("Sectional Details",clear_on_submit=True):
-        # file = st.file_uploader("Upload Your book", type="pdf",disabled=True)
+    uploaded_file = st.file_uploader("Upload Your book", type="pdf",accept_multiple_files=True)
+    pages = []
+    for i in uploaded_file:
+        mi = uploaded_file[0].file_id
+        with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+            tmp_file.write(i.getvalue())
+            tmp_file_path = tmp_file.name
+            loader = PyPDFLoader(tmp_file_path)
+            pages += loader.load_and_split()
+            print("  Total Pages :",len(pages),"")
+    if pages:
+        with st.spinner(text="In progress..."):
+            embeddings = OpenAIEmbeddings()
+            book_content_vectorstore = FAISS.from_documents(pages, embeddings)
+            file  = f'pkl/{mi}.pkl'
+            if not os.path.exists(file):
+                files  = os.listdir('pkl')
+                for i in files:
+                    os.remove(f'pkl/{i}')
+                with open(file, 'wb') as f:
+                    pickle.dump(book_content_vectorstore, f)
+
+    with st.form("Sectional Details",clear_on_submit=True,key='kp'):
         Section = st.radio("Select Section Name", ["A", "B", "C"],horizontal = True,index=0,key='section')
         show_A = False
         show_B = False 
@@ -68,15 +154,8 @@ if st.session_state.my_dict:
         input_dict = st.session_state.my_dict
         question_paper_result = generate_questions(input_dict)
         st.write("Generate Question Paper")
-        # question_paper_result = "done still not work"
         st.write(question_paper_result)
         if st.download_button("Download as Text File",data=question_paper_result,type='primary',key='non'): 
             st.success("Text file downloaded!")  
             
     
-
-
-
-
-
-
